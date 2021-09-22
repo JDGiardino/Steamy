36502baa80d5c39398151053b684e41191240026
JDGiardino <JDGiardino@gmail.com>
2021-09-21 22:57:02 -0400

    Move achievement formatting logic to own file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 import steam
-import re
-import string
 from src import steam_api
 from steam.client import SteamClient
+from src.utils import formatter
 
 
 def return_game_id() -> int:
@@ -15,16 +14,10 @@ def return_game_id() -> int:
 
 
 def return_rarest_achievement() -> str:
-    rarest_achievement = steam_api.get_rarest_achievement(return_game_id()) #This is a dict
-
-    numbers = r'[0-9]'
-    rarest_achievement_name = re.sub(numbers, '', rarest_achievement['name'])
-    rarest_achievement_name = rarest_achievement_name.replace("_"," ")
-    rarest_achievement_name = string.capwords(rarest_achievement_name)
-
-    rarest_achievement_percent = round(rarest_achievement['percent'], 2)
-
-    return f"The rarest achievement is {rarest_achievement_name} which {rarest_achievement_percent}% of players unlocked"
+    achievement = steam_api.get_rarest_achievement(return_game_id()) #This is a dict
+    achievement_name = formatter.format_achievement_name(achievement['name'])
+    achievement_percent = formatter.format_achievement_percent(achievement['percent'])
+    return f"The rarest achievement is {achievement_name} which {achievement_percent}% of players unlocked"
 
 
 def main():
