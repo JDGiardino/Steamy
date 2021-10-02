18310a290fd525d22f33f5caa0d3f8f3239a351e
JDGiardino <JDGiardino@gmail.com>
2021-10-02 10:47:11 -0400

    main.py function order to match steam_api

--- src/main.py
+++ src/main.py
@@ -14,6 +14,14 @@ def return_game_id() -> Union [int,None]:
     else:
         return game_id
 
+
+def return_rarest_achievement() -> str:
+    achievement = steam_api.get_rarest_achievement(return_game_id()) #This is a dict
+    achievement_name = formatter.format_achievement_name(achievement['name'])
+    achievement_percent = formatter.format_achievement_percent(achievement['percent'])
+    return f"The rarest achievement is {achievement_name} which {achievement_percent}% of players unlocked"
+
+
 def return_users_game_playtime() -> str:
     user = input('What user would you like to look up? ')
     user_id = steam_api.get_steam_id(user)
@@ -36,13 +44,6 @@ def return_users_total_platime() -> str:
     else:
         total_playtime = formatter.format_users_total_playtime(steam_api.get_users_total_playtime(user_id))
         return f"{user} has a grand total of {total_playtime} hours played on Steam!"
-
-
-def return_rarest_achievement() -> str:
-    achievement = steam_api.get_rarest_achievement(return_game_id()) #This is a dict
-    achievement_name = formatter.format_achievement_name(achievement['name'])
-    achievement_percent = formatter.format_achievement_percent(achievement['percent'])
-    return f"The rarest achievement is {achievement_name} which {achievement_percent}% of players unlocked"
 
 
 def main():
