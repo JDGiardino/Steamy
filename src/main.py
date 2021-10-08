d210c1834f767c46a82aeeb28b396a8f891a0240
Joe Giardino <JDGiardino@gmail.com>
2021-10-08 00:56:11 -0400

    Refactor as Discord bot (#7)
    * Adding function for getting users total steam playtime
    
    * main.py function order to match steam_api
    
    * Refactor as Discord bot
    
    * One way to error handle @bot.commands
    
    * Includes error handling for all commands

--- src/main.py
+++ src/main.py
@@ -1,43 +1,48 @@
 import steam
+import discord
+from discord.ext import commands
+import os
 from typing import Union
 from src import steam_api
 from src.utils import formatter
 from steam.steamid import SteamID
 
+DISCORD_BOT_TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
+if not DISCORD_BOT_TOKEN:
+    raise Exception('No DISCORD_BOT_TOKEN provided')
 
-def return_game_id() -> Union [int,None]:
-    game = input('What game would you like to look up? ')
+bot = commands.Bot(command_prefix = '$')
+
+
+def return_game_id(game: str) -> Union [int,None]:
     game_id = steam_api.get_game_id(game)
     if game_id is None:
-        print("Game not found")
-        main()
+        return "Game not found"
     else:
         return game_id
 
 
-def return_rarest_achievement() -> str:
-    achievement = steam_api.get_rarest_achievement(return_game_id()) #This is a dict
+def return_rarest_achievement(game:str) -> str:
+    achievement = steam_api.get_rarest_achievement(return_game_id(game)) #This is a dict
     achievement_name = formatter.format_achievement_name(achievement['name'])
     achievement_percent = formatter.format_achievement_percent(achievement['percent'])
     return f"The rarest achievement is {achievement_name} which {achievement_percent}% of players unlocked"
 
 
-def return_users_game_playtime() -> str:
-    user = input('What user would you like to look up? ')
+def return_users_game_playtime(user: str, game: str) -> str:
     user_id = steam_api.get_steam_id(user)
     if user_id is None:
         return "User not found or has a private profile"
     else:
-        game = return_game_id()
-        if steam_api.get_users_game_playtime(user_id, game) is None:
+        game_id = return_game_id(game)
+        if steam_api.get_users_game_playtime(user_id, game_id) is None:
             total_hours = 0
         else:
-            total_hours = formatter.format_users_game_playtime(steam_api.get_users_game_playtime(user_id, game))
-        return f"{user} has a total of {total_hours} hours played!"
+            total_hours = formatter.format_users_game_playtime(steam_api.get_users_game_playtime(user_id, game_id))
+        return f"{user} has a total of {total_hours} hours played on {game}!"
 
 
-def return_users_total_platime() -> str:
-    user = input('What user would you like to look up? ')
+def return_users_total_platime(user) -> str:
     user_id = steam_api.get_steam_id(user)
     if user_id is None:
         return "User not found or has a private profile"
@@ -47,26 +52,30 @@ def return_users_total_platime() -> str:
 
 
 def main():
-    request = input('What Steam request would you like to make? Options :\n '
-                    'Get game ID\n '
-                    'Get a user\'s playtime hours on a game\n '
-                    'Get a user\'s total playtime hours on Steam\n '
-                    'Get rarest achievement in a game\n ')
-    if request == "Get game ID":
-        print(return_game_id())
-        main()
-    elif request == "Get a user\'s playtime hours on a game":
-        print(return_users_game_playtime())
-        main()
-    elif request == "Get a user\'s total playtime hours on Steam":
-        print(return_users_total_platime())
-        main()
-    elif request == "Get rarest achievement in a game":
-        print(return_rarest_achievement())
-        main()
-    else:
-        print('That is not a valid request')
-        main()
+    @bot.command()
+    async def game_id(ctx, arg: str):
+        await ctx.send(return_game_id(arg))
+
+    @bot.command()
+    async def rarest_achievement(ctx, arg: str):
+        await ctx.send(return_rarest_achievement(arg))
+
+    @bot.command()
+    async def users_game_playtime(ctx, arg1: str, arg2: str):
+        await ctx.send(return_users_game_playtime(arg1, arg2))
+
+    @bot.command()
+    async def users_total_playtime(ctx, arg: str):
+        await ctx.send(return_users_total_platime(arg))
+
+    @bot.event
+    async def on_command_error(ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send('You did not include an argument with the command')
+        else:
+            await ctx.send('There was an error')
+
+    bot.run(DISCORD_BOT_TOKEN)
 
 
 if __name__ == "__main__":
